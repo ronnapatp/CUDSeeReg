7beb0584f7d4f51c87c7a7e6cf583a2aae58b837
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2023-12-31 12:15:51 +0700

    Add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,17 @@
+import Container from "./container"
+export default function footer(){
+    return(
+        <footer className="border-t mt-10">
+        <Container>
+            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+                <div className="sm:flex sm:items-center sm:justify-between">
+                        <span className="self-center font-semibold whitespace-nowrap">© 2024 CUDSeeReg Team</span>
+                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                        <a href="/privacy">Privacy Policy</a>
+                    </ul>
+                </div>
+            </div>
+            </Container>
+        </footer>
+    )
+}
