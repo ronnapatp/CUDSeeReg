91faff560d0683b8e8845e56010bf9e551c7d1ad
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2024-02-11 11:01:02 +0700

    Fix bug

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,5 @@
 import Container from "./container"
+import Link from "next/link"
 export default function footer(){
     return(
         <footer className="border-t mt-10">
@@ -7,7 +8,7 @@ export default function footer(){
                 <div className="sm:flex sm:items-center sm:justify-between">
                         <span className="self-center font-semibold whitespace-nowrap">© 2024 CUDSeeReg Team</span>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                        <a href="/privacy">Privacy Policy</a>
+                        <Link href="/privacy">Privacy Policy</Link>
                     </ul>
                 </div>
             </div>
